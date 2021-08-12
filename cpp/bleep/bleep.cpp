d6a2e1124aa970172135787b415847298fe1c7db
jean k <acalto@outlook.com>
2021-08-12 23:13:59 +0200

    ok

--- cpp/bleep/bleep.cpp
+++ cpp/bleep/bleep.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 
-std::string word = "broccoli";
-std::string text = "My favorite things are broccoli, and I love to eat a few boccoli at lunch. In my kitchen garden there are always plenty of broccoli";
-void bleep (std::string word, std::string &text);
+char word[] = "broccoli";
+char text[] = "My favorite things are broccoli, and I love to eat a few broccoli at lunch. In my kitchen garden there are always plenty of broccoli";
+char bleep[]= "********";
+int bleep_size = sizeof(bleep)-1;
 
-int main (){
-    std::cout << text << "\n";
-  bleep (word, text);
-  std::cout << text << "\n";
+ int main (){
+  char * ptr;
+  std::cout << text << '\n';
+  do {
+    ptr = strstr(  text,  word);  
+    if (ptr) {
+      strncpy ( ptr, bleep, bleep_size );
+    }
+  } while (ptr != NULL);
+  std::cout << text << '\n';
   return 0;
-}
-
-void bleep (std::string word, std::string &text){
-
-  std::cout << text << "\n";
-  text = "ha ha ha";
-  std::cout << text << "\n";
-
 }
