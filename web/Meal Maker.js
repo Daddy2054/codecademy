ba180c25d12aaafade7b215c73a829b261b0b337
jean k <acalto@outlook.com>
2021-08-16 19:17:35 +0200

    ok

---
+++ web/Meal Maker.js
@@ -0,0 +1,26 @@
+let menu = {
+    _courses : {
+        appetizers: [],
+        get appetizers() {
+
+        },
+        set appetizers() {
+
+        },
+        mains: [],
+        get mains() {
+
+        },
+        set mains() {
+
+        },
+        desserts: [],
+        get desserts() {
+
+        },
+        set desserts() {
+
+        },
+        
+    },
+}
