56536d85ac99cb28b6e2a967802fdd20ded43794
jean k <acalto@outlook.com>
2021-08-17 14:00:56 +0200

    ok

---
+++ web/Team Stats.js
@@ -0,0 +1,20 @@
+const team = {
+    _players: [
+      {
+        firstname: 'David',
+        lastName: 'Baxes',
+        age: 11
+      };
+      {
+        firstname: 'John',
+        lastName: 'Baxtin',
+        age: 12
+      };
+      {
+        firstname: 'Martin',
+        lastName: 'Golod',
+        age: 13
+      };
+    ],
+    _games: [],
+  };
