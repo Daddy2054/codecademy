31a31a328bda46c20f1ecedf74343bd3eb59a886
jean k <acalto@outlook.com>
2021-08-17 14:04:33 +0200

    ok

---
+++ web/Team Stats/Team Stats.js
@@ -0,0 +1,40 @@
+const team = {
+    _players: [
+      {
+        firstname: 'David',
+        lastName: 'Baxes',
+        age: 11
+      },
+      {
+        firstname: 'John',
+        lastName: 'Baxtin',
+        age: 12
+      },
+      {
+        firstname: 'Martin',
+        lastName: 'Golod',
+        age: 13
+      }
+    ],
+    _games: [
+        {
+            opponent: 'Tacos',
+            teamPoints: 39,
+            opponentPoints: 27
+
+        },
+        {
+            opponent: 'Mares',
+            teamPoints: 29,
+            opponentPoints: 17
+
+        },
+        {
+            opponent: 'Locos',
+            teamPoints: 41,
+            opponentPoints: 37
+
+        },
+    ],
+
+  };
