31ca17dcd7f33086f1e9fd19ceb0c2dff2d87a98
jean k <acalto@outlook.com>
2021-09-04 23:57:42 +0200

    some code

--- python/C4.py
+++ python/C4.py
@@ -61,36 +61,67 @@ def you_win(board, color): #check if it is win move
     # 2. checks colums
     # 3. check diagonals 6 + 6
     win_color = [color,color,color,color]
-    board_rows = [[board[0][0]+ board[0][1]+ board[0][2]+
-                  board[0][3]+ board[0][4]+ board[0][5]+ board[0][6]],
-                [board[1][0]+ board[1][1]+ board[1][2]+
-                  board[1][3]+ board[1][4]+ board[1][5]+ board[1][6]],
-                [board[0][0], board[0][1], board[0][2],
-                  board[0][3], board[0][4], board[0][5], board[0][6]],
-                [board[0][0], board[0][1], board[0][2],
-                  board[0][3], board[0][4], board[0][5], board[0][6]],
-                [board[0][0], board[0][1], board[0][2],
-                  board[0][3], board[0][4], board[0][5], board[0][6]],
-                [board[0][0], board[0][1], board[0][2],
-                  board[0][3], board[0][4], board[0][5], board[0][6]],
-                
-                ]
+    
+    board_rows = []
     for  i in range(0,6): #check rows...
-        if board[i].find(win_color) == -1:
+        board_rows.append([board[i][0]+ board[i][1]+
+                         board[i][2]+ board[i][3]+ 
+                         board[i][4]+ board[i][5]])
+    for i in range(0,6):                      
+        if board_rows[i].find(win_color) == -1:
             continue    #check next row
 
+    board_columns = []    
     for i in range(0,8): #check columns
-        board_column = []    
-        for y in range(0,6):
-            board_column.append(board[y][i])
-        if board_column.find(win_color) == -1:
+        board_columns.append([board[0][i]+ board[0][i]+
+                         board[1][i]+ board[2][i]+ 
+                         board[3][i]+ board[4][i]+
+                         board[5][i]+ board[6][i]])
+    for y in range(0,8):
+        if board_columns.find(win_color) == -1:
             continue    #check next column
     
     #  check diagonals 6 + 6
-    board_diag = [board[3][0], 
-                board[2][1], 
-                board[1][2],
-                board[0][3]]  
+    board_diags = []
+    board_diags.append([board[3][0]+ 
+                board[2][1]+
+                board[1][2]+
+                board[0][3]]) # diag 1
+    board_diags.append([board[4][0]+ 
+                board[3][1]+
+                board[2][2]+
+                board[1][3]+
+                board[0][4]])   # diag2
+    board_diags.append([board[5][0]+ 
+                board[4][1]+
+                board[3][2]+
+                board[2][3]+
+                board[1][4]+
+                board[0][5]])   #diag3
+    board_diags.append([board[5][1]+ 
+                board[4][2]+
+                board[3][3]+
+                board[2][4]+
+                board[1][5]+
+                board[0][6]])   #diag 4
+    board_diags.append([board[5][2]+ 
+                board[4][3]+
+                board[3][2]+
+                board[2][1]+
+                board[1][0]])   # diag 5
+    board_diags.append([board[5][3]+ 
+                board[4][1]+
+                board[3][1]+
+                board[2][2]+
+                board[1][3]+
+                board[0][4]])
+    board_diags.append([board[4][0]+ 
+                board[2][1]+
+                board[1][2]+
+                board[0][3]]) # diag 1
+               
+                
+                
 
 #print(pick_color())
 while True:
