d497a7f86b1e9fec4f65de5c7b96e6fe16315729
Jean <acalto2@gmail.com>
2022-06-17 01:20:19 +0200

    ok

---
+++ text-adventure-solution/my-text-adv.cpp
@@ -0,0 +1,197 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "The Castration of Uranus and the Birth of Aphrodite\n\n";
+    std::cout << "We must now return to Hesiod(Theogony156-206),and his account of the birth of the mighty goddess of love, Aphrodite (the Roman Venus). The children of Uranus and Ge (the twelve Titans, including the last-born, wily Cronus, who especially hated his father; the Cyclopes; and the Hecatonchires) all were despised by their father from the beginning.\n";
+    std::cout << "\n";
+    std::cout << "    /^\\ \n";
+    std::cout << "    |  | \n";
+    std::cout << "    |  | \n";
+    std::cout << "    |  | \n";
+    std::cout << "  /\\|  |/\ \n";
+    std::cout << " |  |  |  | \n";
+    std::cout << "  \\/|__|\\/ \n";
+
+    std::cout << "\n";
+    std::cout << "As each of his children was born, Uranus...\n";
+    std::cout << "...   _ _ _ _   ...\n";
+    std::cout << "And he delighted in his wickedness.\n";
+    std::cout << "What Uranus did?\n";
+
+    char choice1;
+    
+    for (int i = 0; i < 3 && choice1 != 'A' && choice1 != 'B' && choice1 != 'C'; i++) {
+
+        std::cout << "A. hid them all in the depths of Ge and did not allow them to emerge into the light. \n";
+        std::cout << "B. eat them one by one when he was hungry.\n";
+        std::cout << "C. raised them to be good children.\n";
+        std::cout << "Enter capital letter A,B or C:\n";
+        std::cin >> choice1;
+
+    }
+
+    switch (choice1) {
+
+        case 'A':
+            std::cout << "But huge Earth in her distress groaned within and devised a crafty and evil scheme.\n";
+            break;
+
+        case 'B':
+            std::cout << "When Uranus ate all his children, he become well-fed.\n";
+            std::cout << "\n";
+            std::cout << "THE END\n";
+            return 0;
+
+        case 'C':
+            std::cout << "When childred of Uranus became adults they loved ad cared for their father.\n";
+            std::cout << "\n";
+            std::cout << "THE END\n";
+            break;
+
+        default:
+            std::cout << "Sorry, you haven't entered a valid choice.\n";
+            std::cout << "Goodbye.\n";
+            return 0;
+
+    }
+
+    std::cout << "At once she created gray adamant and fashioned a great sickle and confided in her dear children. Sorrowing in her heart she urged them as follows: \n";
+    std::cout << "\n";
+    std::cout << "A. 'My dear children, today I prepared an yammy cake and I invite you to dinner.\n";
+    std::cout << "B. 'My children born of a presumptuous father, if you are willing to obey, we shall punish his evil insolence. For he was the first to devise shameful actions.' (156-166)\n";
+    std::cout << "Enter capital letter A or B:\n";
+     char choice2;
+    
+    for (int i = 0; i < 3 && choice2 != 'A' && choice2 != 'B'; i++) {
+
+        std::cin >> choice2;
+
+    }
+
+    if (choice2 != 'A' && choice2 != 'B') {
+
+        std::cout << "Sorry, you haven't entered a valid choice.\n";
+        std::cout << "Goodbye.\n";
+        return 0;
+
+    } else if (choice2 == 'A') {
+
+        std::cout << "Please, take with you all your friends as well!\n";
+        std::cout << "THE END\n";
+        return 0;
+
+    }
+
+    std::cout << "Thus she spoke. Fear seized them all and not one answered. But great and wily Cronus took courage and spoke to his dear mother: \n";
+    std::cout << "\n";    
+    std::cout << "A. 'I shall undertake and accomplish the deed, since I do not care about our abominable father. For he was the first to devise shameful actions.' (167-172)\n";
+    std::cout << "B. 'My dear Mother! Thank You for all you courage from all of us. We prefer to stay aside, though.'\n";
+    std::cout << "Enter capital letter A or B:\n";
+    std::cout << "\n";
+
+    char choice3;
+    
+    for (int i = 0; i < 3 && choice3 != 'A' && choice3 != 'B'; i++) {
+
+        std::cin >> choice3;
+
+    }
+
+    if (choice3 != 'A' && choice3 != 'B') {
+
+        std::cout << "Sorry, you haven't entered a valid choice.\n";
+        std::cout << "Goodbye.\n";
+        return 0;
+
+    } else if (choice3 == 'B') {
+
+        std::cout << "When he spoke that, Uranus made him his preffered son.\n";
+        std::cout << "\n";
+        std::cout << "THE END\n";
+        return 0;
+
+    }
+
+    std::cout << "Thus he spoke. And huge Earth rejoiced greatly in her heart. She hid him in an ambush and placed in his hands the sickle with jagged teeth and revealed the whole plot to him. Great Uranus came leading on night, and, desirous of love,\n";
+        std::cout << "\n";
+    std::cout << "A. lay on Ge,spreading himself over her completely. \n";
+    std::cout << "B. started speaking in verse about his Big Love.\n";
+    std::cout << "C. make a present of the new Moon.\n";
+    std::cout << "\n";
+    std::cout << "Enter capital letter A, B or C:\n";
+    std::cout << "\n";
+
+    char choice4;
+    
+    for (int i = 0; i < 3 && choice4 != 'A' && choice4 != 'B' && choice4 != 'C'; i++) {
+        std::cin >> choice4;
+
+    }
+
+    if (choice4 != 'A' && choice4 != 'B' && choice4 != 'C') {
+
+        std::cout << "Sorry, you haven't entered a valid choice.\n";
+        std::cout << "Goodbye.\n";
+        return 0;
+
+    } else if (choice4 == 'A') {
+
+        std::cout << "And his son from his ambush reached out with his left hand and in his right he seized hold of the huge sickle with jagged teeth and swiftly cut off ...\n";
+        std::cout << "\n";
+        
+    } else if (choice4 == 'B') {
+
+        std::cout << "And everyone become happy and they lived long.\n";
+        std::cout << "\n";
+        std::cout << "THE END\n";
+    } else if (choice4 == 'C') {
+
+        std::cout << "But huge Earth had already a Moon and she declined the offer.\n";
+        std::cout << "\n";
+        std::cout << "THE END\n";
+
+    }
+
+    std::cout << "A. the genitals of his own dear father and threw them so that they fell behind him. \n";
+    std::cout << "B. his own leg, because of darkness in the cave.\n";
+    std::cout << "C. the head of his brother who was near helping in the fight\n";
+        std::cout << "\n";
+
+    char choice5;
+
+    for (int i = 0; i < 3 && choice5 != 'A' && choice5 != 'B' && choice5 != 'C'; i++) {
+        std::cin >> choice5;
+    }
+
+    switch (choice5) {
+
+        case 'A':
+            std::cout << "And they did not fall from his hand in vain. Earth received all the bloody drops that fell and in the course of the seasons bore the strong Erinyes and the mighty giants (shining in their armor and carrying long spears in their hands) and nymphs of ash trees (called Meliae on the wide earth). (173-187)\n";
+        std::cout << "\n";
+            break;
+
+        case 'B':
+            std::cout << "He said 'I'm not a loser, I'm not a loser, I'm not a loser. \n";
+        std::cout << "\n";
+            std::cout << "THE END\n";
+            return 0;
+
+        case 'C':
+            std::cout << "Everyone become very sad and his mother started to cry.\n";
+        std::cout << "\n";
+            std::cout << "THE END\n";
+            return 0;
+
+        default:
+            std::cout << "Sorry, you haven't entered a valid choice.\n";
+            std::cout << "Goodbye.\n";
+            return 0;
+    }
+
+    std::cout << "When first he had cut off the genitals with the adamant and cast them from the land on the swelling sea, they were carried for a long time on the deep. And white foam arose about from the immortal flesh and in it a maiden grew. First she was brought to holy Cythera, and then from there she came to sea-girt Cyprus. And she emerged a dread and beautiful goddess and grass rose under her slender feet. (188-195)\n";
+    std::cout << "Gods and human beings call her Aphrodite, and the foam-born goddess because she grew amid the foam (aphros), and Cytherea of the beautiful crown because she came to Cythera, and Cyprogenes because she arose in Cyprus washed by the waves. She is called too Philommedes (genital-loving) because she arose from the genitals. Eros attended her and beautiful desire followed her when she was born and when she first went into the company of the gods. From the beginning she has this honor, and among human beings and the immortal gods she wins as her due the whispers of girls, smiles, deceits, sweet pleasure, and the gentle delicacy of love. (195-206)\n";
+    std::cout << "\n";
+            std::cout << "THE END\n";
+    
+}
